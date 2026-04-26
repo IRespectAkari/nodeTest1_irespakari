9c00ea99af559aa1b7b70c9640843408ec8d21e2
IRespectAkari <145774048+IRespectAkari@users.noreply.github.com>
2026-04-26 19:49:42 +0900

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,3 +1,3 @@
-set PATH=C:\Programming\node-v24.15.0-win-x64;C:\Programming\node-v24.15.0-win-x64;C:\windows\system32;C:\windows;C:\windows\System32\Wbem;C:\windows\System32\WindowsPowerShell\v1.0\;C:\windows\System32\OpenSSH\;C:\Program Files (x86)\Fujitsu\FJAgent\Core\bin;C:\RUN-shortcut;C:\Program Files\dotnet\;C:\software\Git\cmd;C:\Program Files\PowerToys\DSCModules\;C:\Users\keigo\AppData\Local\Microsoft\WindowsApps;C:\software\Microsoft VS Code\bin;C:\Users\keigo\AppData\Local\Python\bin;C:\Users\keigo\AppData\Local\JetBrains\Toolbox\scripts; 
+set PATH=C:\Programming\node-v24.15.0-win-x64;%PATH%
 doskey ls=dir 
 chcp 65001
