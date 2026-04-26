90358638a4c3a5f25fa6e75a0863519f9974ecf0
keigo <x.10.a.s.0820keigo@gmail.com>
2026-04-27 02:54:47 +0900

    update: app.js update to add modify feature

--- app.js
+++ app.js
@@ -8,11 +8,38 @@ const db = new Database('my_database.db');
 
 // publicフォルダの中身（HTMLなど）をブラウザに見せる設定
 app.use(express.static('public'));
+// ブラウザから送られてくるJSONデータを読み解くための設定
+app.use(express.json());
 
-// 【重要】ブラウザから「/api/users」にアクセスされたら、DBの中身をJSONで返す
+// Read: 全ユーザー取得（GET）
 app.get('/api/users', (req, res) => {
   const users = db.prepare('SELECT * FROM users').all();
-  res.json(users); // ここでJSONとして返却！
+  res.json(users);
+});
+
+// Create: 新規ユーザー追加（POST）
+app.post('/api/users', (req, res) => {
+  const { name, age } = req.body;// ブラウザから送られた名前と年齢を受け取る
+  const insert = db.prepare('INSERT INTO users (name, age) VALUEAS (?, ?)');
+  const info = insert.run(name, age);
+  res.json({ success: true, id: info.lastInsertRowid });
+});
+
+// Update: ユーザー情報の更新（PUT）
+app.put('/api/users:id', (req, res) => {
+  const id = req.params.id;// URLの末尾のIDを受け問る
+  const { name, age } = req.body;
+  const update = db.prepare('UPDATE users SET name = ?, age = ? WHERE id = ?');
+  update.run(name, age, id);
+  res.json({ success: true });
+});
+
+// Delete: ユーザー削除（DELETE）
+app.delete('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+  const del = db.prepare('DELETE FROM users WHERE id = ?');
+  del.run(id);
+  res.json({ success: true });
 });
 
 // サーバーを起動する
